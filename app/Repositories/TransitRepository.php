64db9579dfa130d0f418841b0579c37f597e6c21
ivalrivall <ivalrival95@gmail.com>
2021-04-18 16:00:51 +0700

    add number in transits table

--- app/Repositories/TransitRepository.php
+++ app/Repositories/TransitRepository.php
@@ -7,6 +7,7 @@ use App\Models\Pickup;
 
 use Carbon\Carbon;
 use InvalidArgumentException;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
 
 class TransitRepository
 {
@@ -27,6 +28,13 @@ class TransitRepository
      */
     public function saveTransitRepo($data = [])
     {
+        $config = [
+            'table' => 'transits',
+            'length' => 1,
+            'field' => 'number',
+            'prefix' => 'T'.Carbon::now('Asia/Jakarta')->format('ymd'),
+            'reset_on_prefix_change' => true
+        ];
         $transit = $this->transit;
         $transit->pickup_id = $data['pickupId'];
         $transit->status = $data['status'];
@@ -34,6 +42,7 @@ class TransitRepository
         $transit->notes = $data['notes'];
         $transit->created_by = $data['userId'];
         $transit->updated_by = $data['userId'];
+        $transit->number = IdGenerator::generate($config);
         $transit->save();
         return $transit;
     }
@@ -56,5 +65,102 @@ class TransitRepository
             'draft' => $draft
         ];
         return $data;
+    }
+
+    /**
+     * get outstanding transit pickup
+     * status only pending and received false
+     * @param array $data
+     */
+    public function getOutstandingPickupRepo($data = [])
+    {
+        $perPage = $data['perPage'];
+        $page = $data['page'];
+        $sort = $data['sort'];
+        $customer = $data['customer'];
+        $general = $data['general'];
+        $pickupOrderNo = $data['pickupOrderNo'];
+        $requestPickupDate = $data['requestPickupDate'];
+        $pickupPlanNo = $data['pickupPlanNo'];
+
+        $transit = $this->transit->with('pickup')->where('status', 'pending')->where('received', false);
+
+        if (empty($perPage)) {
+            $perPage = 10;
+        }
+
+        // if (!empty($sort['field'])) {
+        //     $order = $sort['order'];
+        //     if ($order == 'ascend') {
+        //         $order = 'asc';
+        //     } else if ($order == 'descend') {
+        //         $order = 'desc';
+        //     } else {
+        //         $order = 'desc';
+        //     }
+        //     switch ($sort['field']) {
+        //         case 'name':
+        //             $transit = $transit->sortable([
+        //                 'name' => $order
+        //             ]);
+        //             break;
+        //         case 'id':
+        //             $transit = $transit->sortable([
+        //                 'id' => $order
+        //             ]);
+        //             break;
+        //         case 'pickup_plan_id':
+        //             $transit = $transit->sortable([
+        //                 'pickup_plan_id' => $order
+        //             ]);
+        //             break;
+        //         case 'picktime':
+        //             $transit = $transit->sortable([
+        //                 'picktime' => $order
+        //             ]);
+        //             break;
+        //         case 'number':
+        //             $transit = $transit->sortable([
+        //                 'number' => $order
+        //             ]);
+        //             break;
+        //         default:
+        //             $transit = $transit->sortable([
+        //                 'number' => 'desc'
+        //             ]);
+        //             break;
+        //     }
+        // }
+
+        // if (!empty($customer)) {
+        //     $transit = $transit->where('name', 'ilike', '%'.$customer.'%');
+        // }
+
+        // if (!empty($pickupOrderNo)) {
+        //     $transit = $transit->where('number', 'ilike', '%'.$pickupOrderNo.'%');
+        // }
+
+        // if (!empty($requestPickupDate)) {
+        //     $transit = $transit->whereDate('picktime', date($requestPickupDate));
+        // }
+
+        // if (!empty($pickupPlanNo)) {
+        //     $transit = $transit->whereHas('pickupPlan', function($q) use ($pickupPlanNo) {
+        //         $q->where('number', 'ilike', '%'.$pickupPlanNo.'%');
+        //     });
+        // }
+
+        // if (!empty($general)) {
+        //     $transit = $transit
+        //         ->where('name', 'ilike', '%'.$general.'%')
+        //         ->orWhere('number', 'ilike', '%'.$general.'%')
+        //         ->orWhereHas('pickupPlan', function($q) use ($general) {
+        //             $q->where('number', 'ilike', '%'.$general.'%');
+        //         });
+        // }
+
+        $result = $transit->paginate($perPage);
+
+        return $result;
     }
 }
