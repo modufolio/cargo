068a7adb607d4073db9b2b1de8f91a5181477061
ivalrivall <ivalrival95@gmail.com>
2021-05-22 14:16:46 +0700

    adding validation for calculate price

--- app/Services/BillService.php
+++ app/Services/BillService.php
@@ -39,20 +39,68 @@ class BillService {
     /**
      * Calculate price.
      *
-     * @param Array $data
      * @return mixed
      */
-    public function calculatePrice($items = [], $route = [])
+    public function calculatePriceService($items = [], $route, $promo)
     {
         if (empty($items)) {
             throw new InvalidArgumentException('Item tidak ditemukan');
         }
+
         if (empty($route)) {
             throw new InvalidArgumentException('Rute tidak masuk dalam jangkauan');
         }
 
+        $validator = Validator::make($route, [
+            'origin'                => 'bail|required',
+            'destination_district'  => 'bail|required',
+            'destination_city'      => 'bail|required',
+            'price'                 => 'bail|required|numeric',
+            'price_motorcycle'      => 'bail|required|numeric',
+            'price_car'             => 'bail|required|numeric',
+            'minimum_weight'        => 'bail|required|numeric'
+        ]);
+
+        if ($validator->fails()) {
+            DB::rollback();
+            throw new InvalidArgumentException($validator->errors()->first());
+        }
+
+        $validator = Validator::make($promo, [
+            'discount'      => 'bail|required|numeric',
+            'discount_max'  => 'bail|required|numeric',
+            'min_value'     => 'bail|required|numeric',
+            'start_at'      => 'bail|required',
+            'end_at'        => 'bail|required',
+            'max_used'      => 'bail|required|numeric',
+            'code'          => 'bail|required',
+            'scope'         => 'bail|required'
+        ]);
+
+        if ($validator->fails()) {
+            DB::rollback();
+            throw new InvalidArgumentException($validator->errors()->first());
+        }
+
+        foreach ($items as $key => $value) {
+            $validator = Validator::make($value, [
+                'unit'          => 'bail|required',
+                'unit_count'    => 'bail|required|numeric',
+                'type'          => 'bail|required',
+                'name'          => 'bail|required',
+                'weight'        => 'bail|required|numeric',
+                'volume'        => 'bail|required|numeric',
+                'service_id'    => 'bail|nullable|present'
+            ]);
+
+            if ($validator->fails()) {
+                DB::rollback();
+                throw new InvalidArgumentException($validator->errors()->first());
+            }
+        }
+
         try {
-            $result = $this->billRepository->calculatePrice($items, $route);
+            $result = $this->billRepository->calculatePriceRepo($items, $route, $promo);
         } catch (Exception $e) {
             Log::info($e->getMessage());
             throw new InvalidArgumentException('Gagal memperkirakan biaya');
