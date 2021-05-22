1fabcda3b5b2ddb8da425cf05b47310afc42d516
ivalrivall <ivalrival95@gmail.com>
2021-05-22 16:34:11 +0700

    [deprecated] save drop

--- app/Repositories/PickupRepository.php
+++ app/Repositories/PickupRepository.php
@@ -1569,40 +1569,40 @@ class PickupRepository
 
     /**
      * Save Drop Order
-     *
+     * DEPRECATED
      * @param array $data
      * @param Promo $promo
      * @return Pickup
      */
-    public function createDropAdminRepo($data, $promo)
-    {
-        $config = [
-            'table' => 'pickups',
-            'length' => 12,
-            'field' => 'number',
-            'prefix' => 'P'.Carbon::now('Asia/Jakarta')->format('ymd'),
-            'reset_on_prefix_change' => true
-        ];
-        $pickup = new $this->pickup;
+    // public function createDropAdminRepo($data, $promo)
+    // {
+    //     $config = [
+    //         'table' => 'pickups',
+    //         'length' => 12,
+    //         'field' => 'number',
+    //         'prefix' => 'P'.Carbon::now('Asia/Jakarta')->format('ymd'),
+    //         'reset_on_prefix_change' => true
+    //     ];
+    //     $pickup = new $this->pickup;
 
-        $pickup->fleet_id           = $data['fleetId'];
-        $pickup->user_id            = $data['userId'];
-        $pickup->promo_id           = $promo['id'] ?? null;
-        $pickup->name               = $data['name'];
-        $pickup->phone              = $data['phone'];
-        $pickup->sender_id          = $data['senderId'];
-        $pickup->receiver_id        = $data['receiverId'];
-        $pickup->debtor_id          = $data['debtorId'];
-        $pickup->notes              = $data['notes'];
-        $pickup->picktime           = $data['picktime'];
-        $pickup->created_by         = $data['userId'];
-        $pickup->status             = 'applied';
-        $pickup->number             = IdGenerator::generate($config);
-        $pickup->is_drop            = true;
-        $pickup->save();
+    //     $pickup->fleet_id           = $data['fleetId'];
+    //     $pickup->user_id            = $data['userId'];
+    //     $pickup->promo_id           = $promo['id'] ?? null;
+    //     $pickup->name               = $data['name'];
+    //     $pickup->phone              = $data['phone'];
+    //     $pickup->sender_id          = $data['senderId'];
+    //     $pickup->receiver_id        = $data['receiverId'];
+    //     $pickup->debtor_id          = $data['debtorId'];
+    //     $pickup->notes              = $data['notes'];
+    //     $pickup->picktime           = $data['picktime'];
+    //     $pickup->created_by         = $data['userId'];
+    //     $pickup->status             = 'applied';
+    //     $pickup->number             = IdGenerator::generate($config);
+    //     $pickup->is_drop            = true;
+    //     $pickup->save();
 
-        return $pickup;
-    }
+    //     return $pickup;
+    // }
 
     /**
      * Save Pickup by admin
